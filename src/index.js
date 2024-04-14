dae0ae86c8f41df64e0d5525dbafe59c83605c33
Amir Hossein <amir73khakbaz@gmail.com>
2024-04-14 13:41:23 +0330

    fix error default import

--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
-import { createContext, useContext, useState } from "react"
+import { createContext, useContext, useState } from "react";
 
 const SharedStateContext = createContext();
+const SharedStateProvider = ({ state, children }) => {
+  const [data, setData] = useState(state);
 
-export const SharedStateProvider = ({ state, children}) => {
-    const [data, setData] = useState(state);
+  return React.createElement(
+    SharedSimpleStateContext.Provider,
+    { value: { data, setData } },
+    children
+  );
+};
+const useSharedStateHook = () => {
+  const data = useContext(SharedStateContext);
 
-    return React.createElement(SharedSimpleStateContext.Provider, { value: { data, setData } }, children)
-}
+  return [data.data, data.setData];
+};
 
-export const useSharedStateHook = () => {
-    const data = useContext(SharedStateContext);
-
-    return [data.data, data.setData];
-}
+export default { useSharedStateHook, SharedStateProvider };
