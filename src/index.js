7d1e0b303111b3385bd8d1a80239b52e2d72306f
Amir Hossein <amir73khakbaz@gmail.com>
2024-04-14 14:24:43 +0330

    clean code

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
-const SharedStateContext = createContext();
+import { createContext, useContext, useState } from "react";
 
+const SharedStateContext = createContext();
 const SharedStateProvider = ({ state, children }) => {
   const [data, setData] = useState(state);
 
   return React.createElement(
-    SharedStateContext.Provider,
+    SharedSimpleStateContext.Provider,
     { value: { data, setData } },
     children
   );
 };
+const useSharedStateHook = () => {
+  const data = useContext(SharedStateContext);
+
+  return [data.data, data.setData];
+};
+
+export default { useSharedStateHook, SharedStateProvider };
